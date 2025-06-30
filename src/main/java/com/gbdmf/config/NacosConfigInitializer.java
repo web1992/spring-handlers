b0f26e8107724b6ae72da5db718ef371756b1aa0
erbao.wang <erbao.wang@zhaogang.com>
2025-06-30 14:49:54 +0800

    ADD NacosConfigInitializer.java

--- src/main/java/com/gbdmf/config/NacosConfigInitializer.java
+++ src/main/java/com/gbdmf/config/NacosConfigInitializer.java
@@ -1,4 +1,4 @@
-//package com.gbdmf.config;
+//package com.internet.configcenter.environment;
 //
 //import com.alibaba.nacos.api.NacosFactory;
 //import com.alibaba.nacos.api.config.ConfigService;
@@ -8,9 +8,10 @@
 //import com.alibaba.nacos.spring.context.annotation.config.NacosValueAnnotationBeanPostProcessor;
 //import com.alibaba.nacos.spring.context.event.config.NacosConfigReceivedEvent;
 //import com.internet.configcenter.util.NacosContetxUtils;
+//import org.apache.commons.lang3.StringUtils;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
-//import org.springframework.beans.BeansException;
+//import org.springframework.context.ApplicationContext;
 //import org.springframework.context.ApplicationContextInitializer;
 //import org.springframework.context.ApplicationListener;
 //import org.springframework.context.ConfigurableApplicationContext;
@@ -19,6 +20,8 @@
 //import org.springframework.core.env.MapPropertySource;
 //import org.springframework.core.env.MutablePropertySources;
 //
+//import java.io.IOException;
+//import java.io.StringReader;
 //import java.util.HashMap;
 //import java.util.Map;
 //import java.util.Properties;
@@ -30,7 +33,8 @@
 //
 //    private static final Logger log = LoggerFactory.getLogger(NacosConfigInitializer.class);
 //
-//    private NacosValueAnnotationBeanPostProcessor nacosValueAnnotationBeanPostProcessor;
+//    //private NacosValueAnnotationBeanPostProcessor nacosValueAnnotationBeanPostProcessor;
+//    private Map<ApplicationContext, NacosValueAnnotationBeanPostProcessor> nacosValueAnnotationBeanPostProcessorMap = new HashMap<>();
 //    private ConfigService configService;
 //    private static final String GROUP = "DEFAULT_GROUP";
 //    public static String curDataId = null;
@@ -44,9 +48,6 @@
 //            // 2. 首次加载配置
 //            loadAndMergeConfig(applicationContext);
 //
-//            // 3. 添加监听器实现动态刷新
-//            addConfigListener(applicationContext);
-//
 //            // 注册一个监听器，在容器刷新完成后获取 Bean
 //            applicationContext.addApplicationListener(new ApplicationListener<ContextRefreshedEvent>() {
 //                @Override
@@ -55,12 +56,17 @@
 //                    // ApplicationContext 存在多个，并且存在父子关系，所以需要判断是否存在这个 bean
 //                    if (event.getApplicationContext().containsBeanDefinition(NacosValueAnnotationBeanPostProcessor.BEAN_NAME)) {
 //                        try {
-//                            if (null != NacosConfigInitializer.this.nacosValueAnnotationBeanPostProcessor) {
+//                            NacosValueAnnotationBeanPostProcessor nacosValueAnnotationBeanPostProcessor = nacosValueAnnotationBeanPostProcessorMap.get(event.getApplicationContext());
+//                            if (null != nacosValueAnnotationBeanPostProcessor) {
 //                                log.info("{} NacosValueAnnotationBeanPostProcessor is already registered in application context", LOG_PREFIX);
 //                                return;
 //                            }
-//                            NacosConfigInitializer.this.nacosValueAnnotationBeanPostProcessor = event.getApplicationContext().getBean(NacosValueAnnotationBeanPostProcessor.class);
-//                        } catch (BeansException e) {
+//                            nacosValueAnnotationBeanPostProcessorMap.put(event.getApplicationContext(), event.getApplicationContext().getBean(NacosValueAnnotationBeanPostProcessor.class));
+//                            // addConfigListener 在这里执行，支持多个 ApplicationContext
+//                            // 添加监听器实现动态刷新
+//                            NacosConfigInitializer.this.addConfigListener(applicationContext);
+//
+//                        } catch (Exception e) {
 //                            log.error("Failed to get NacosValueAnnotationBeanPostProcessor", e);
 //                        }
 //                    } else {
@@ -91,9 +97,19 @@
 //    private void loadAndMergeConfig(ConfigurableApplicationContext context) throws NacosException {
 //        // 获取配置内容
 //        String configContent = configService.getConfig(curDataId, GROUP, 5000);
-//
 //        // 解析配置内容（这里以简单 key=value 为例）
-//        Map<String, Object> configMap = parseConfigContent(configContent);
+//        Map<String, Object> configMap = parsePropertiesText(configContent);
+//
+//        String dataIds = NacosContetxUtils.getDataIds();
+//        if (StringUtils.isNotBlank(dataIds)) {
+//            String[] dataIdList = dataIds.split(",");
+//            for (String dataId : dataIdList) {
+//                String configContentCur = configService.getConfig(dataId, GROUP, 5000);
+//                if (StringUtils.isNotBlank(configContentCur)) {
+//                    configMap.putAll(parsePropertiesText(configContentCur));
+//                }
+//            }
+//        }
 //
 //        // 将配置合并到 Spring Environment
 //        ConfigurableEnvironment environment = context.getEnvironment();
@@ -107,7 +123,26 @@
 //        propertySources.addFirst(new MapPropertySource("nacosConfig", configMap));
 //    }
 //
-//    private Map<String, Object> parseConfigContent(String content) {
+//    public Map<String, Object> parsePropertiesText(String propertiesText) {
+//        Properties properties = new Properties();
+//        try {
+//            properties.load(new StringReader(propertiesText));
+//        } catch (IOException e) {
+//            log.error("Failed to parse properties text", e);
+//        }
+//
+//        Map<String, Object> result = new HashMap<>();
+//        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+//            String key = entry.getKey().toString().trim();
+//            // 过滤掉 # 开头的注释行
+//            if (!key.startsWith("#")) {
+//                result.put(key, entry.getValue().toString().trim());
+//            }
+//        }
+//        return result;
+//    }
+//
+///*    private Map<String, Object> parseConfigContent(String content) {
 //        // 简单解析 key=value 格式
 //        Map<String, Object> configMap = new HashMap<>();
 //        for (String line : content.split("\n")) {
@@ -117,10 +152,10 @@
 //            }
 //        }
 //        return configMap;
-//    }
+//    }*/
 //
 //    private void addConfigListener(ConfigurableApplicationContext context) throws NacosException {
-//        log.info("{} addConfigListener curDataId {}", LOG_PREFIX, curDataId);
+//        log.info("{} addConfigListener curDataId {} ApplicationName {}", LOG_PREFIX, curDataId, context.getApplicationName());
 //        configService.addListener(curDataId, GROUP, new Listener() {
 //            @Override
 //            public Executor getExecutor() {
@@ -132,12 +167,23 @@
 //                // 配置变更时重新加载
 //                log.info("Nacos config changed, reloading...");
 //                try {
-//                    loadAndMergeConfig(context);
-//                    if (null != nacosValueAnnotationBeanPostProcessor) {
-//                        nacosValueAnnotationBeanPostProcessor.setEnvironment(context.getEnvironment());
-//                        // 让 支持 NacosValue 注解 支持自动刷新
-//                        nacosValueAnnotationBeanPostProcessor.onApplicationEvent(new NacosConfigReceivedEvent(configService, curDataId, GROUP, configInfo, ConfigType.PROPERTIES.getType()));
-//                    }
+//
+//                    nacosValueAnnotationBeanPostProcessorMap.forEach((context, nacosValueAnnotationBeanPostProcessor) -> {
+//                        try {
+//                            loadAndMergeConfig((ConfigurableApplicationContext) context);
+//                        } catch (NacosException e) {
+//                            log.error("Failed to reload Nacos configuration", e);
+//                        }
+//                        //NacosValueAnnotationBeanPostProcessor nacosValueAnnotationBeanPostProcessor = nacosValueAnnotationBeanPostProcessorMap.get(context);
+//                        if (null != nacosValueAnnotationBeanPostProcessor) {
+//                            nacosValueAnnotationBeanPostProcessor.setEnvironment(context.getEnvironment());
+//                            // 让 支持 NacosValue 注解 支持自动刷新
+//                            nacosValueAnnotationBeanPostProcessor.onApplicationEvent(new NacosConfigReceivedEvent(configService, curDataId, GROUP, configInfo, ConfigType.PROPERTIES.getType()));
+//                        } else {
+//                            log.warn("{} NacosValueAnnotationBeanPostProcessor is not registered in application context", LOG_PREFIX);
+//                        }
+//                    });
+//
 //                } catch (Exception e) {
 //                    log.error("Failed to reload Nacos configuration", e);
 //                }
